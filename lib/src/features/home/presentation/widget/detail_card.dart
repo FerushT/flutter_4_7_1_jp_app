32865089df510908b8c699b15327736547347acc
FerushT <ferush82@gmail.com>
2024-07-19 16:08:18 +0200

    change to return Stack

--- lib/src/features/home/presentation/widget/detail_card.dart
+++ lib/src/features/home/presentation/widget/detail_card.dart
@@ -1,26 +1,208 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter_4_7_1_jp_app/src/features/home/presentation/widget/my_circle_button.dart';
+import 'package:flutter_4_7_1_jp_app/src/features/home/presentation/widget/my_gradiant_elevated_button.dart';
+import 'package:flutter_4_7_1_jp_app/src/features/home/presentation/widget/my_ingredients_widget.dart';
+import 'package:flutter_4_7_1_jp_app/src/features/home/presentation/widget/my_reviews_widget.dart';
+import 'package:flutter_sficon/flutter_sficon.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class DetailCard extends StatelessWidget {
   const DetailCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        elevation: 0,
-        color: Colors.transparent,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(35),
-          side: BorderSide(
-            color: Colors.white.withOpacity(0.20),
-            width: 1,
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          height: 753,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.6, 1],
+              colors: [
+                Color.fromARGB(255, 46, 43, 35),
+                Color.fromARGB(255, 56, 66, 66),
+              ],
+            ),
           ),
         ),
-        child: SizedBox(
-          height: 320,
-          width: 320,
+        Positioned(
+          top: -133,
+          child: SizedBox(
+            height: 400,
+            child: Image.asset(
+              "assets/grafiken/cat_cupcakes_3D.png",
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          top: 20,
+          right: 15,
+          height: 30,
+          width: 30,
+          child: MyCircleButton(
+            child: SFIcon(
+              SFIcons.sf_xmark,
+              fontSize: 15,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Positioned(
+          top: 175,
+          left: 25,
+          right: 25,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(35),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 18.0, sigmaY: 18.0),
+              child: Container(
+                height: 350,
+                padding: const EdgeInsets.all(25),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(35),
+                  border: Border.all(
+                      color: const Color.fromARGB(255, 193, 170, 218),
+                      width: 0.2),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const Icon(
+                          Icons.favorite_border,
+                          size: 15,
+                        ),
+                        Text(" 200",
+                            textAlign: TextAlign.end,
+                            style: TextStyle(
+                                fontSize: 13,
+                                fontFamily: "Inter",
+                                color: Colors.white.withOpacity(0.8),
+                                letterSpacing: -0.5,
+                                fontWeight: FontWeight.w400)),
+                      ],
+                    ),
+                    const Text(
+                      "Mogli's Cup",
+                      style: TextStyle(
+                          fontSize: 25,
+                          letterSpacing: -0.5,
+                          fontWeight: FontWeight.w900),
+                    ),
+                    const Text(
+                      "Lorem ipsum dolor sit amet consectetur. Non feugiat imperdiet a vel sit at amet. Mi accumsan feugiat magna aliquam feugiat ac et. Pulnivar hendrerit id arcu at sed etiam semper mi hendrerit. Id aliquet quis quam.",
+                      style: TextStyle(fontSize: 10),
+                      textAlign: TextAlign.center,
+                    ),
+                    const Text(
+                      "₳ 8.99",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w900),
+                    ),
+                    const Divider(
+                      color: Color.fromARGB(255, 154, 154, 154),
+                    ),
+                    const Row(
+                      children: [
+                        MyIngredientsWidget(),
+                        Expanded(child: SizedBox()),
+                        MyReviewsWidget(),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 155,
+          left: 21,
+          child: ToggleSwitch(
+            initialLabelIndex: 2,
+            totalSwitches: 3,
+            labels: const ["Small", "Medium", "Large"],
+            onToggle: (index) {},
+            activeBgColor: const [
+              Color.fromARGB(255, 113, 113, 113),
+            ],
+            inactiveBgColor: const Color.fromARGB(255, 80, 80, 80),
+            activeFgColor: Colors.white,
+            fontSize: 11.3,
+            dividerMargin: 9,
+            radiusStyle: true,
+            minHeight: 30,
+            customWidths: const [
+              55,
+              65,
+              55,
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 155,
+          right: 100,
+          height: 30,
+          width: 30,
+          child: MyCircleButton(
+            child: SFIcon(
+              SFIcons.sf_minus,
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        const Positioned(
+          bottom: 155,
+          right: 70,
+          child: Text(
+            "1",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+          ),
+        ),
+        Positioned(
+          bottom: 155,
+          right: 21,
+          height: 30,
+          width: 30,
+          child: MyCircleButton(
+            child: SFIcon(
+              SFIcons.sf_plus,
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 70,
+          left: 21,
+          right: 21,
+          child: MyGradientElevatedButton(
+              height: 50,
+              width: 185,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              colorBegin: const Color.fromARGB(255, 251, 123, 223),
+              colorEnd: const Color.fromARGB(255, 241, 152, 152),
+              borderRadius: 10,
+              text: "Add to order for ₳ 8.99",
+              textSize: 17),
+        ),
+      ],
     );
   }
 }
