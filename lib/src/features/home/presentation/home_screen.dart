f4012f75f554e86bd8c9444d605008bc83106296
FerushT <ferush82@gmail.com>
2024-07-21 02:55:06 +0200

    changed color

--- lib/src/features/home/presentation/home_screen.dart
+++ lib/src/features/home/presentation/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_4_7_1_jp_app/src/features/home/presentation/widget/item_card_big.dart';
 import 'package:flutter_4_7_1_jp_app/src/features/home/presentation/widget/item_card_small.dart';
 import 'package:flutter_4_7_1_jp_app/src/features/home/presentation/widget/category_buttons.dart';
-import 'package:flutter_4_7_1_jp_app/src/features/home/presentation/widget/my_gradiant_elevated_button.dart';
 import 'package:flutter_sficon/flutter_sficon.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -47,18 +46,21 @@ class HomeScreen extends StatelessWidget {
                                 SFIcons.sf_takeoutbag_and_cup_and_straw,
                                 fontSize: 11,
                                 fontWeight: FontWeight.bold,
-                                color: Colors.white.withOpacity(0.8),
+                                color: Colors.white.withOpacity(0.5),
                               ),
-                              Text(" All categories ",
-                                  style: TextStyle(
-                                      color: Colors.white.withOpacity(0.8),
-                                      letterSpacing: -0.5,
-                                      fontSize: 12)),
+                              Text(
+                                " All categories ",
+                                style: TextStyle(
+                                    color: Colors.white.withOpacity(0.5),
+                                    letterSpacing: -0.5,
+                                    fontSize: 11,
+                                    fontWeight: FontWeight.bold),
+                              ),
                               SFIcon(
                                 SFIcons.sf_chevron_down,
                                 fontSize: 11,
                                 fontWeight: FontWeight.bold,
-                                color: Colors.white.withOpacity(0.8),
+                                color: Colors.white.withOpacity(0.5),
                               ),
                             ],
                           ),
@@ -68,13 +70,14 @@ class HomeScreen extends StatelessWidget {
                         style: TextStyle(
                             color: Color.fromARGB(255, 0, 0, 0),
                             letterSpacing: -0.5,
-                            fontSize: 12),
+                            fontSize: 11,
+                            fontWeight: FontWeight.w800),
                       )),
                       buildCategoryButton(
                           Text(
                             "Sweet",
                             style: TextStyle(
-                                color: Colors.white.withOpacity(0.8),
+                                color: Colors.white.withOpacity(0.5),
                                 letterSpacing: -0.5,
                                 fontSize: 12),
                           ),
@@ -83,7 +86,7 @@ class HomeScreen extends StatelessWidget {
                           Text(
                             "Dry Fruits",
                             style: TextStyle(
-                                color: Colors.white.withOpacity(0.8),
+                                color: Colors.white.withOpacity(0.5),
                                 letterSpacing: -0.5,
                                 fontSize: 12),
                           ),
@@ -96,16 +99,18 @@ class HomeScreen extends StatelessWidget {
                 ),
                 const ItemCardBig(),
                 const SizedBox(height: 55),
-                const Text("We Recommend",
-                    style: TextStyle(
-                        height: 0,
-                        fontSize: 15,
-                        fontFamily: "Inter",
-                        color: Colors.white,
-                        letterSpacing: -0.5,
-                        fontWeight: FontWeight.w900)),
+                const Text(
+                  " We Recommend",
+                  style: TextStyle(
+                      height: 0,
+                      fontSize: 15,
+                      fontFamily: "Inter",
+                      color: Colors.white,
+                      letterSpacing: -0.2,
+                      fontWeight: FontWeight.w900),
+                ),
                 const SizedBox(
-                  height: 15,
+                  height: 10,
                 ),
                 const SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
@@ -114,11 +119,13 @@ class HomeScreen extends StatelessWidget {
                       ItemCardSmall(
                           imagePath: "assets/grafiken/cat_cupcakes_3D.png",
                           imageTitle: "Mogli's Cup",
-                          imageDescription: "Strawberry ice cream"),
+                          imageDescription: "Strawberry ice cream",
+                          imageLikes: "200"),
                       ItemCardSmall(
                           imagePath: "assets/grafiken/Ice_cream.png",
                           imageTitle: "Balu's Cup",
-                          imageDescription: "Pistachio ice cream"),
+                          imageDescription: "Pistachio ice cream",
+                          imageLikes: "100"),
                     ],
                   ),
                 ),
