deb0e0ce71e105e27196aa9f640c88ecead593fb
FerushT <ferush82@gmail.com>
2024-07-21 03:03:00 +0200

    change in GestureDetector

--- lib/src/features/home/presentation/widget/my_circle_button.dart
+++ lib/src/features/home/presentation/widget/my_circle_button.dart
@@ -10,23 +10,20 @@ class MyCircleButton extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(50),
       clipBehavior: Clip.antiAlias,
-      child: Material(
-        color: Colors.transparent,
-        borderRadius: BorderRadius.circular(50),
-        child: InkWell(
-          onTap: () {},
-          highlightColor: const Color.fromARGB(180, 142, 134, 255),
-          child: Ink(
-            decoration: BoxDecoration(
-                color: const Color.fromARGB(120, 47, 71, 62),
-                borderRadius: BorderRadius.circular(50),
-                border: Border.all(
-                    color: Colors.white.withOpacity(0.7),
-                    width: 0.3,
-                    strokeAlign: BorderSide.strokeAlignInside)),
-            child: Center(
-              child: child,
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(120, 47, 71, 62),
+            borderRadius: BorderRadius.circular(50),
+            border: Border.all(
+              color: Colors.white.withOpacity(0.7),
+              width: 0.3,
+              strokeAlign: BorderSide.strokeAlignInside,
             ),
+          ),
+          child: Center(
+            child: child,
           ),
         ),
       ),
